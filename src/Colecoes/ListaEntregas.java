95d978d86ff52413357f9b6a98a4f7cc4e6c8a0a
Lucas Martins <l.martins90@edu.pucrs.br>
2020-10-23 22:58:46 -0300

    correcao-datas-do-Entrega.toString-e-outras-melhorias

--- src/Colecoes/ListaEntregas.java
+++ src/Colecoes/ListaEntregas.java
@@ -1,9 +1,7 @@
 package Colecoes;
 
 import Entities.Entrega;
-
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -35,25 +33,9 @@ public class ListaEntregas {
         return null;
     }
 
-    public List<Entrega> getEntregas(String descricao){
-        List<Entrega> subList = new ArrayList<>();
-        for (Entrega en: listaEntregas) {
-            if(en.getDescricao().contains(descricao)){
-                subList.add(en);
-            }
-        }
-        return subList;
-    }
-
-
     public List<Entrega> getNaoRetiradas(){
         Predicate<Entrega> naoRetiradas = entrega -> entrega.getDataRetirada() == null;
         return listaEntregas.stream().filter(naoRetiradas).collect(Collectors.toList());
-    }
-
-    public void atualizarEntrega(Entrega entrega) {
-        int indice = listaEntregas.indexOf(entrega);
-        listaEntregas.set(indice, entrega);
     }
 
     public int getSize(){
